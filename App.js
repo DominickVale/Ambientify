a2b70fd1e77987064ac66abf4affcd9632a97a4d
Dominick Veil <dominickveil@gmail.com>
2019-11-05 14:23:01 +0100

    Added react-native-vector-icons

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 
 const App = () => {
+
   return (
     <>
       <SafeAreaView>
-        <Text> Test </Text>
+        <Text> Test1 </Text>
       </SafeAreaView>
     </>
   );
