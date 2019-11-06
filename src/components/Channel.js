71eace9c1f7a375af7134e1eee50a7bf76a806e5
Dominick Veil <dominickveil@gmail.com>
2019-11-06 20:18:38 +0100

    Added basic setup

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Text, Button, View } from 'react-native'
 import Slider from '@react-native-community/slider'
+import { Audio } from 'expo-av'
+import { initChannel } from '../actions'
 /**
  * TODO:
- * Add Load button
- * Add Play&Stop button
- * Add volume slider
+ * Add test button functionalities x redux
  * @param {*} props 
  */
 
-const loadHandler = () => { }
-
-const toggleSoundHandler = () => { }
-
 const Channel = (props) => {
+  const dispatch = useDispatch();
+  const channel = useSelector(state => state.channels[props.id])
+
+  useEffect(() => {
+    dispatch(initChannel(props.id, new Audio.Sound()))
+    return () => {
+      cleanup();
+    };
+  }, [])
+
+  useEffect(() => {
+    console.log("Channel loaded")
+  }, [channel.loaded])
+
+  const cleanup = async () => { }
+  const loadHandler = () => { console.log(channel) }
+  const toggleSoundHandler = () => { }
+
   return (
     <>
       <View>
