24ef85cb1ae997904d34d12cb12dfd4b51e1279e
Dominick Veil <dominickveil@gmail.com>
2019-11-06 15:22:01 +0100

    Added basic controls

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, Button, View } from 'react-native'
+import Slider from '@react-native-community/slider'
+/**
+ * TODO:
+ * Add Load button
+ * Add Play&Stop button
+ * Add volume slider
+ * @param {*} props 
+ */
+
+const loadHandler = () => { }
+
+const toggleSoundHandler = () => { }
 
 const Channel = (props) => {
   return (
     <>
-      <Text>
-        Channel {props.id}
-      </Text>
+      <View>
+        <Text>
+          Channel {props.id}
+        </Text>
+        <Button title="Load" onPress={loadHandler} />
+        <Button title=" > " onPress={toggleSoundHandler} />
+        <Slider minimumValue={0}
+          maximumValue={1}
+          step={0.05}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#000000" />
+      </View>
     </>
   )
 }
