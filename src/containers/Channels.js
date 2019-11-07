8b22e4b78555d17362a6210715f2282b34c8b1aa
Dominick Veil <dominickveil@gmail.com>
2019-11-07 17:34:41 +0100

    Add Audio initialization

--- src/containers/Channels.js
+++ src/containers/Channels.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text } from 'react-native'
 import Channel from '../components/Channel'
+import { Audio } from 'expo-av'
+
+async function initSounds() {
+  await Audio.setAudioModeAsync({
+    allowsRecordingIOS: false,
+    staysActiveInBackground: true,
+    interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+    playsInSilentModeIOS: true,
+    shouldDuckAndroid: false,
+    interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+    playThroughEarpieceAndroid: true,
+  });
+}
 
 const Channels = (props) => {
+
+  useEffect(() => {
+    initSounds();
+  }, [])
+
   return (
     <>
       <Text> Channels container</Text>
