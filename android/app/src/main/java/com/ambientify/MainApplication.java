4f915e50be73a7e0ef274a980f11b4e49a0acc67
Dominick Veil <dominickveil@gmail.com>
2019-11-12 22:34:47 +0100

    Add react-native-minimize and fix app closing on back button in main screen

--- android/app/src/main/java/com/ambientify/MainApplication.java
+++ android/app/src/main/java/com/ambientify/MainApplication.java
@@ -6,6 +6,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.farzadmarius.rnminimizeapp.RNMinimizeAppPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
--- src/screens/Mixer.js
+++ src/screens/Mixer.js
@@ -5,12 +5,22 @@
  *  implement
  */
 
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { Text, BackHandler } from 'react-native'
+import RNMinimizeApp from 'react-native-minimize'
 
 import Channels from '../containers/Channels'
 
 const Mixer = (props) => {
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      RNMinimizeApp.minimizeApp();
+      return true
+    })
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress')
+    };
+  }, [])
   return (
     <>
       <Text>
