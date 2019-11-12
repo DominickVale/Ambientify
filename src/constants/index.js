e41f45ad678ed6d51a46da94e718a7204e48d78e
Dominick Veil <dominickveil@gmail.com>
2019-11-12 18:08:56 +0100

    Add sound category hierarchy + update comments

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,16 +4,29 @@
  * 
  * obj SOUND_FILES:
  *  List of sound files with their final name as key. The key must be equal to the actual file name.
- *  The sounds will be loaded by accessing SOUND_FILES[id].
+ *  The sounds will be loaded by accessing SOUND_FILES[CATEGORY][Key].
+ * 
+ *  Underscores ( _ ) will be translated to spaces. E.g    Test_Sound   ->  Test Sound
  */
 
 export const NUMBER_OF_CHANNELS = 12;
 
 export const SOUND_FILES = {
-  crows: require('#ambientify-sounds/crows.ogg'),
-  dark: require('#ambientify-sounds/dark.ogg'),
-  wind: require('#ambientify-sounds/wind.ogg'),
-  wildlife: require('#ambientify-sounds/wildlife.ogg')
+  NATURE: {
+    wind: require('#ambientify-sounds/wind.ogg'),
+  },
+  ANIMALS: {
+    crows: require('#ambientify-sounds/crows.ogg'),
+    wildlife: require('#ambientify-sounds/wildlife.ogg')
+  },
+  DARK: {
+    dark: require('#ambientify-sounds/dark.ogg'),
+  },
+  MUSIC: {
+    test_sound1: require('#ambientify-sounds/test_sound1.ogg'),
+    test_sound2: require('#ambientify-sounds/test_sound2.ogg'),
+  }
+
 }
 export const COLORS = {
   icons: '#5B636C',
