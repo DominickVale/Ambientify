aa57ec21fd358d7c7b877b2fd79d44e7a3571781
Dominick Veil <dominickveil@gmail.com>
2019-11-15 13:47:30 +0100

    add deletePreset action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,17 @@
 /**
  * TODO:
- * Preset actions
+ * Settings 
  */
 
 export const LOAD_SOUND = 'LOAD_SOUND';
 export const LOAD_SOUND_ERR = 'LOAD_SOUND_ERR';
 export const PLAY_SOUND = 'PLAY_SOUND'
 export const STOP_SOUND = 'STOP_SOUND'
-export const SET_VOLUME = 'SET_VOLUME' //Only used by preset dispatches
+export const SET_VOLUME = 'SET_VOLUME'
 
 export const ADD_PRESET = 'ADD_PRESET'
 export const LOAD_PRESET = 'LOAD_PRESET'
-export const REMOVE_PRESET = 'REMOVE_PRESET'
+export const DELETE_PRESET = 'DELETE_PRESET'
 
 export const loadSound = (channelId, newSound, newSoundCategory, newSoundName) => ({
   type: LOAD_SOUND,
@@ -44,9 +44,13 @@ export const addPreset = name => async (dispatch, getState) => {
 
 export const loadPreset = name => async (dispatch, getState) => {
   const newChannelsState = { ...getState().presets[name] }
-  console.log(newChannelsState)
   dispatch({
     type: LOAD_PRESET,
     payload: { name, newChannelsState }
   })
 }
+
+export const deletePreset = name => ({
+  type: DELETE_PRESET,
+  payload: { name }
+})
