fd02f76316ad68dd0c5b4cbde6e6ae42e760f05b
Dominick Veil <dominickveil@gmail.com>
2019-11-15 14:52:07 +0100

    Add navigate to homescreen  after preset selection

--- src/components/PresetItem.js
+++ src/components/PresetItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import { withNavigation } from 'react-navigation'
 
 import { View, Text, Button } from 'react-native'
 import { loadPreset, deletePreset } from '../actions'
@@ -9,10 +10,13 @@ const PresetItem = (props) => {
 
   return (
     <View>
-      <Button title={props.presetName} onPress={() => dispatch(loadPreset(props.presetName))} />
+      <Button title={props.presetName} onPress={() => {
+        dispatch(loadPreset(props.presetName))
+        props.navigation.navigate('Mixer')
+      }} />
       <Button title="X" onPress={() => dispatch(deletePreset(props.presetName))} />
     </View>
   )
 }
 
-export default PresetItem
+export default withNavigation(PresetItem)
