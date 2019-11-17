3998a2c2f2a993541b591e27acde974a58f5eceb
Dominick Veil <dominickveil@gmail.com>
2019-11-17 17:18:16 +0100

    Add react-native-background-timer

--- android/app/src/main/java/com/ambientify/MainApplication.java
+++ android/app/src/main/java/com/ambientify/MainApplication.java
@@ -31,14 +31,11 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             @SuppressWarnings("UnnecessaryLocalVariable")
             List<ReactPackage> packages = new PackageList(this).getPackages();
-            // Packages that cannot be autolinked yet can be added manually here, for
-            // example:
-            // packages.add(new MyReactNativePackage());
+
             // Add unimodules
             List<ReactPackage> unimodules = Arrays
                     .<ReactPackage>asList(new ModuleRegistryAdapter(mModuleRegistryProvider));
             packages.addAll(unimodules);
-
             return packages;
         }
 
