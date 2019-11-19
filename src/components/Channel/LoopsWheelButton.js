76b4a471276936fd52543a120df80ce91206820a
Dominick Veil <dominickveil@gmail.com>
2019-11-19 18:23:50 +0100

    Move toggle Shuffle in loops wheel

--- src/components/Channel/LoopsWheelButton.js
+++ src/components/Channel/LoopsWheelButton.js
@@ -3,36 +3,25 @@ import { View, Button } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import { withNavigation } from 'react-navigation'
 
-import { toggleLooping } from '../../actions'
-
 let wheelData = []
 const getWheelData = () => {
   let values = [];
-  for (let i = 1; i < 59; i++) {
+  for (let i = 1; i < 60; i++) {
     values.push(`${i}`);
   }
   wheelData = values
 }
 
 const LoopsWheelButton = ({ channelId, navigation }) => {
-  const dispatch = useDispatch()
-  const { soundObject, looping, file } = useSelector(state => state.channels[channelId])
+  const { looping, loops } = useSelector(state => state.channels[channelId])
   useEffect(() => {
     getWheelData();
   }, [])
 
-  const toggleRandomShuffle = async () => {
-    if (!looping) {
-      let { durationMillis } = await soundObject.getStatusAsync()
-      soundObject.playFromPositionAsync(durationMillis - 10)
-    }
-    dispatch(toggleLooping(channelId))
-  }
   return (
     <>
       <View>
-        <Button title="xx - xm" onPress={() => navigation.push('LoopsWheel', { wheelData, channelId })} />
-        <Button title={looping ? 'x' : '∞'} onPress={toggleRandomShuffle} />
+        <Button title={looping ? `${loops.times}x - ${loops.minutes}m` : '∞'} onPress={() => navigation.push('LoopsWheel', { wheelData, channelId })} />
       </View>
     </>
   )
--- src/screens/LoopsWheel.js
+++ src/screens/LoopsWheel.js
@@ -5,20 +5,39 @@ import { View, Text, Button, BackHandler } from 'react-native'
 import { withNavigation } from 'react-navigation'
 
 import { useBackHandler } from '../utils/'
-import { setLoops } from '../actions'
+import { setLoops, toggleLooping, playSound } from '../actions'
 
 const LoopsWheel = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const { soundObject, file, looping } = useSelector(state => state.channels[navigation.getParam('channelId')])
+
   const wheelData = navigation.getParam('wheelData')
   const channelId = navigation.getParam('channelId')
 
-  const dispatch = useDispatch();
   const [timesWheelState, setTimesWheelState] = useState(1)
   const [minutesWheelState, setMinutesWheelState] = useState(1)
 
   useBackHandler(BackHandler, navigation, () => navigation.goBack());
 
+  const toggleRandomShuffle = async () => {
+    if (file) {
+      /**
+       * Bit of a hacky way to fire didJustFinish event for the soundObject so that it can start shuffling withot having it to play first
+       */
+      let { durationMillis } = await soundObject.getStatusAsync()
+      soundObject.playFromPositionAsync(durationMillis - 1)
+      await soundObject.setIsLoopingAsync(false);
+
+      if (!looping) {
+        dispatch(toggleLooping(channelId))
+        dispatch(playSound(navigation.getParam('channelId')))
+      }
+    }
+  }
+
   const buttonHandler = () => {
     dispatch(setLoops(channelId, { times: timesWheelState + 1, minutes: minutesWheelState + 1 }))
+    if (timesWheelState > 1) toggleRandomShuffle();
     navigation.goBack();
   }
 
@@ -26,8 +45,8 @@ const LoopsWheel = ({ navigation }) => {
     <View onPress={() => navigation.pop()} style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.8)' }}>
       <View style={{ height: "80%", width: '80%', backgroundColor: "white", justifyContent: "flex-start" }}>
         <Text>Choose how many times the sound should be looped</Text>
-        <WheelPicker selectedItem={timesWheelState} data={wheelData} onItemSelected={(value) => setTimesWheelState(value)} />
-        <WheelPicker selectedItem={minutesWheelState} data={wheelData} onItemSelected={(value) => setMinutesWheelState(value)} />
+        <WheelPicker initPosition={0} selectedItem={timesWheelState} data={wheelData} onItemSelected={(value) => setTimesWheelState(value)} />
+        <WheelPicker initPosition={0} selectedItem={minutesWheelState} data={wheelData} onItemSelected={(value) => setMinutesWheelState(value)} />
         <Button title="go" onPress={buttonHandler} />
       </View>
     </View>
