9c169cf44bda05142102259be3f89cfea8844531
Dominick <dominickveil@gmail.com>
2019-11-30 21:39:03 +0100

    Rename SoundCategory to SoundList + refactor + style

---
+++ src/components/SoundList/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native'
+
+import { COLORS } from '../../constants'
+import { InnerContainer } from '../../components/Modal/styles'
+
+export const SoundListContainer = styled.View`
+flex: 2;
+`
--- src/screens/styles/soundPicker.js
+++ src/screens/styles/soundPicker.js
@@ -6,15 +6,17 @@ import { InnerContainer } from '../../components/Modal/styles'
 export const SoundCategoryContainer = styled(InnerContainer)`
 width: ${props => props.width};
 align-self: center;
-border: 1px solid red;
 padding: 0 22px 22px 22px;
 margin: auto 0;
 `
 
 export const SoundCategoryImageContainer = styled.View`
+flex: 1;
 height: ${props => props.height};
 justify-content: center;
+overflow: hidden;
 `
+
 export const SoundCategoryText = styled.Text`
 position: absolute;
 z-index: 1;
@@ -22,4 +24,9 @@ color:${COLORS.headerFore};
 font-family: 'Montserrat-SemiBold';
 align-self: center;
 font-size: 28px;
+`
+
+export const Filler = styled.View`
+flex: 1;
+max-height: 5%;
 `
