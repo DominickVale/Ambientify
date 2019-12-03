0b2e23a5d0dd0912a56340535b3d03756d14cabd
Dominick <dominickveil@gmail.com>
2019-12-03 12:31:29 +0100

    Sound preview implementation + styling

--- src/components/SoundList/SoundItem.js
+++ src/components/SoundList/SoundItem.js
@@ -2,15 +2,47 @@ import React, { useEffect, useState } from 'react'
 import { View, Button } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import { withNavigation } from 'react-navigation'
+import { Audio } from 'expo-av'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
+import { COLORS } from '../../constants'
 import { loadSound } from '../../actions'
 import { SOUND_FILES } from '../../constants/index'
+import { SoundLoadButton, SoundPreviewButton, StyledSoundItem, StyledText } from './styles'
 
 const SoundItem = ({ channelId, navigation, soundName, soundCategory }) => {
   const dispatch = useDispatch();
-  const [loadState, setLoadState] = useState('Load')
-
+  const [playing, setPlaying] = useState(false)
   const customSounds = useSelector(state => state.presets.customSounds)
+
+  const playIcon = <Icon name="play-arrow" size={24} color={COLORS.bigPlayButtonFore} />;
+  const pauseIcon = <Icon name="pause" size={24} color={COLORS.bigPlayButtonFore} />
+
+  useEffect(() => {
+    let soundObject;
+
+    (async () => {
+      if (playing) {
+        try {
+          soundObject = new Audio.Sound()
+          await soundObject.loadAsync(SOUND_FILES[soundCategory][soundName]).then(async () => soundObject.playAsync());
+          soundObject.setOnPlaybackStatusUpdate((playbackStatus) => { if (playbackStatus.didJustFinish) setPlaying(false) });
+        } catch (error) { console.log(error) }
+      } else {
+        if (soundObject) {
+          soundObject.stopAsync();
+          soundObject.unloadAsync();
+        }
+      }
+    })();
+
+    return (() => {
+      if (soundObject) {
+        soundObject.stopAsync();
+        soundObject.unloadAsync();
+      }
+    })
+  }, [playing])
 
   const loadButtonHandler = () => {
     if (soundCategory === 'CUSTOM') {
@@ -22,11 +54,11 @@ const SoundItem = ({ channelId, navigation, soundName, soundCategory }) => {
   }
 
   return (
-    <View>
-      <Button title={soundName} onPress={loadButtonHandler} />
-    </View>
+    <StyledSoundItem>
+      <SoundLoadButton onPress={loadButtonHandler}><StyledText numberOfLines={1}>{soundName}</StyledText></SoundLoadButton>
+      <SoundPreviewButton onPress={() => setPlaying(playing => !playing)}><StyledText>{playing ? pauseIcon : playIcon}</StyledText></SoundPreviewButton>
+    </StyledSoundItem>
   )
 }
 
 export default withNavigation(SoundItem)
-
--- src/components/SoundList/styles.js
+++ src/components/SoundList/styles.js
@@ -1,8 +1,36 @@
 import styled from 'styled-components/native'
 
 import { COLORS } from '../../constants'
-import { InnerContainer } from '../../components/Modal/styles'
 
 export const SoundListContainer = styled.View`
 flex: 2;
+`
+
+export const StyledSoundItem = styled.View`
+justify-content: space-between;
+flex-direction: row;
+background-color: transparent;
+border-radius: 12;
+padding: 0;
+margin-bottom: 6px;
+`
+
+export const SoundLoadButton = styled.TouchableHighlight`
+flex: 4;
+padding: 4px 12px;
+`
+export const SoundPreviewButton = styled.TouchableHighlight`
+flex: 1;
+align-items: center;
+justify-content: center;
+border-radius: 50;
+width: 28px;
+height: 28px;
+max-width: 28px;
+margin: 4px 12px;
+`
+
+export const StyledText = styled.Text`
+color:${COLORS.buttonText};
+font-size: 14px;
 `
