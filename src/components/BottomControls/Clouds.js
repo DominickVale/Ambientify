e2c85fe2973d7371d996e9052ec709ad359a31e6
Dominick <dominickveil@gmail.com>
2019-12-08 11:11:43 +0100

    Revert to only one cloud

--- src/components/BottomControls/Clouds.js
+++ src/components/BottomControls/Clouds.js
@@ -9,17 +9,8 @@ import { CloudsContainer1, CloudsContainer2 } from './styles'
 const Clouds = () => (
   <>
     <CloudsContainer1>
-      <LottieView style={{ width: '100%', height: '100%', transform: [{ translateY: 12 }, { translateX: -130 }] }} source={require('#ambientify-images/cloud2.json')} autoPlay loop />
+      <LottieView style={{ width: '100%', height: '100%' }} source={require('#ambientify-images/cloud2.json')} autoPlay loop />
     </CloudsContainer1>
-    <CloudsContainer1>
-      <LottieView style={{ width: '100%', height: '100%', transform: [{ translateY: 16 }, { translateX: -120 }] }} source={require('#ambientify-images/cloud1.json')} autoPlay loop />
-    </CloudsContainer1>
-    <CloudsContainer2>
-      <LottieView style={{ width: '100%', height: '100%', transform: [{ translateY: 0 }, { translateX: -100 }] }} source={require('#ambientify-images/cloud1.json')} autoPlay loop />
-    </CloudsContainer2>
-    <CloudsContainer2>
-      <LottieView style={{ width: '100%', height: '100%', transform: [{ translateY: 18 }, { translateX: -150 }] }} source={require('#ambientify-images/cloud2.json')} autoPlay loop />
-    </CloudsContainer2>
   </>
 )
 
