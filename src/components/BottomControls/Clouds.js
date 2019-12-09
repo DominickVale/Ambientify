b980e2991f232a57d868b047fbb50b5f05ab60a3
Dominick <dominickveil@gmail.com>
2019-12-09 18:24:46 +0100

    Restyle clouds

--- src/components/BottomControls/Clouds.js
+++ src/components/BottomControls/Clouds.js
@@ -1,15 +1,23 @@
 import React from 'react'
-import LottieView from 'lottie-react-native';
+import { Image, Dimensions } from 'react-native'
 
 import { CloudsContainer1, CloudsContainer2 } from './styles'
 
 /**
  * Should probably fix it.
  */
+const screenWidth = Dimensions.get('screen').width;
+
 const Clouds = () => (
   <>
     <CloudsContainer1>
-      <LottieView style={{ width: '100%', height: '100%' }} source={require('#ambientify-images/cloud2.json')} autoPlay loop />
+      <Image source={require('#ambientify-images/cloud1.png')} style={{ height: 200, width: screenWidth + 150, left: 20 }} />
+    </CloudsContainer1>
+    <CloudsContainer1>
+      <Image source={require('#ambientify-images/cloud2.png')} style={{ height: 190, width: screenWidth + 100 }} />
+    </CloudsContainer1>
+    <CloudsContainer1>
+      <Image source={require('#ambientify-images/cloud3.png')} style={{ height: 210, width: screenWidth + 200, left: -50 }} />
     </CloudsContainer1>
   </>
 )
